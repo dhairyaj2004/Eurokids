92208d1386a122823ddbbcd91d652a4c84e13714
Dhairya Shah <dhairyaj2004@gmail.com>
2025-05-27 20:33:42 +0530

    new template

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -105,26 +105,26 @@ export default function Contact() {
                                 <label for="name" className="hidden">
                                     Full Name
                                 </label>
-                                <input
-                                    type="text"
-                                    name="user_name"
-                                    id="name"
-                                    placeholder="Full Name"
-                                    className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
-                                />
+                               <input
+                  type="name"
+                  name="name"
+                  id="name"
+                  placeholder="Full Name"
+                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+                />
                             </div>
 
                             <div className="flex flex-col mt-2">
                                 <label for="email" className="hidden">
                                     Email
                                 </label>
-                                <input
-                                    type="email"
-                                    name="user_email"
-                                    id="email"
-                                    placeholder="Email"
-                                    className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
-                                />
+                                 <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+                />
                             </div>
 
                             <div className="flex flex-col mt-2">
@@ -139,7 +139,18 @@ export default function Contact() {
                                     className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
                                 />
                             </div>
-
+ <div className="flex flex-col">
+                                <label for="name" className="hidden">
+                                    Message
+                                </label>
+                               <textarea
+                  type="message"
+                  name="message"
+                  id="message"
+                  placeholder="Message"
+                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+                />
+                            </div>
                             <button
                                 type="submit"
                                 className="md:w-32 bg-orange-700 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-orange-600 transition ease-in-out duration-300"
