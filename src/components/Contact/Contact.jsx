0056227fc79be510ad5abca88cdb3cff7782ed02
Dhairya Shah <dhairyaj2004@gmail.com>
2025-05-27 20:36:04 +0530

    css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -139,7 +139,7 @@ export default function Contact() {
                                     className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
                                 />
                             </div>
- <div className="flex flex-col">
+ <div className="flex flex-col mt-2">
                                 <label for="name" className="hidden">
                                     Message
                                 </label>
